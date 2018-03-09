21a30b95e5642ace42ac3ebea77a5913605ccf15
Convex <Convex@Usuario-PC>
2018-03-09 17:46:30 -0300

    Criação de Conexão, Fetchall, Foreach e Exception

--- SchoolOfNet/ConnBD.php
+++ SchoolOfNet/ConnBD.php
@@ -1,10 +1,21 @@
 <?php
-
 try {
-    $conn = new PDO("mysql:host=localhost;dbname=biblioteca","root","root");
-    echo "Conexão realizada com sucesso";
+    $conn = new PDO("mysql:host=localhost;dbname=sakila", "root", "root");
+    // echo "Conexão realizada com sucesso";
     
+    $query = "select * from actor";
+    
+    $stsm = $conn->query($query); // Comando para execução de query
+ // $list = $stsm->fetchAll(PDO::FETCH_OBJ); // Traz todos os registros da consulta para a lista
+    $list = $stsm->fetch(PDO::FETCH_ASSOC); // Traz somente o primeiro registro da consulta para a lista
+    //echo $list[first_name]; // ou  echo $list->first_name
+    //print_r($list);
+    foreach ($conn->query($query) as $atores){
+        echo $atores['first_name'];
+        echo "<br/>";
+    }
+    //
 } catch (PDOException $e) {
-        
-    echo "Erro ao realizar a conexão ".$e->getMessage()." Code: ".$e->getCode();
+    
+    echo "Erro ao realizar a conexão " . $e->getMessage() . " Code: " . $e->getCode();
 }
